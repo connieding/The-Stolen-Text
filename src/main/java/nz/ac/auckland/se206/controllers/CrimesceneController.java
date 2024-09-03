cd73d99d6e6e644c1aca1de80d413e53aac24f5d
Lewis Azzopardi <lazz729@aucklanduni.ac.nz>
2024-09-03 15:09:26 +1200

    :package: Implemented crime scene controller class

---
+++ src/main/java/nz/ac/auckland/se206/controllers/CrimesceneController.java
@@ -0,0 +1,19 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+public class CrimesceneController {
+
+  @FXML private AnchorPane crimescenePane;
+  @FXML private ImageView crimesceneBg;
+  @FXML private Rectangle buttonMap;
+  @FXML private Rectangle clueOne;
+  @FXML private Rectangle clueTwo;
+  @FXML private Rectangle clueThree;
+  @FXML private Text crimesceneHead;
+  @FXML private Rectangle buttonAccuse;
+}
