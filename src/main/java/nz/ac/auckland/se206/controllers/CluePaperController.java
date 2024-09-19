cd0feb9ecde89e1b8c910d007cdcd27258f43479
Connie Ding <cdin152@aucklanduni.ac.nz>
2024-09-19 16:20:39 +1200

    :sparkles: Implemented progression of images on handkerchief click in CluePaperController

--- src/main/java/nz/ac/auckland/se206/controllers/CluePaperController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/CluePaperController.java
@@ -1,3 +1,39 @@
 package nz.ac.auckland.se206.controllers;
 
-public class CluePaperController extends ClueController {}
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class CluePaperController extends ClueController {
+
+  @FXML private ImageView imageAnimate;
+  @FXML private ImageView imageHandkerchief;
+
+  private int clickCount = 0;
+
+  @FXML
+  void handleClickHandkerchief(MouseEvent event) {
+    clickCount++;
+
+    if (clickCount == 1) {
+      imageHandkerchief.setVisible(false);
+    }
+
+    Image[] images = {
+      new Image(getClass().getResourceAsStream("/images/frame1.png")),
+      new Image(getClass().getResourceAsStream("/images/frame2.png")),
+      new Image(getClass().getResourceAsStream("/images/frame3.png")),
+      new Image(getClass().getResourceAsStream("/images/frame4.png")),
+      new Image(getClass().getResourceAsStream("/images/frame5.png")),
+      new Image(getClass().getResourceAsStream("/images/handkerchief.png")),
+    };
+
+    if (clickCount <= images.length) {
+      imageAnimate.setImage(images[clickCount - 1]);
+    } else {
+      clickCount = 0;
+      imageAnimate.setImage(images[clickCount]);
+    }
+  }
+}
