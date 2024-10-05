52b3047263b7f9a4ce7886512f4314a2f9747511
Connie Ding <cdin152@aucklanduni.ac.nz>
2024-10-05 13:50:17 +1300

    :sparkles: Added exit button in start screen

--- src/main/java/nz/ac/auckland/se206/controllers/StartController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StartController.java
@@ -1,8 +1,10 @@
 package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import nz.ac.auckland.se206.App;
 
 public class StartController {
@@ -10,11 +12,16 @@ public class StartController {
   @FXML private Label startButton;
 
   @FXML
-  public void initialize() {
-  }
+  public void initialize() {}
 
   @FXML
   private void onStart() throws IOException {
-    App.openScene(startButton, "crimescene"); 
+    App.openScene(startButton, "crimescene");
+  }
+
+  @FXML
+  void onExit(MouseEvent event) {
+    App.handleWindowClose();
+    Platform.exit();
   }
 }
